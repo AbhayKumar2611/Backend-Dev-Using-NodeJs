a0493debd7535383d059b55ce659ad512f490e87
Abhay Kumar <kumarabhay26111996@gmail.com>
2025-08-15 23:22:46 +0530

    Added tailwind configuration and normal css import

--- Lecture-11 airbnb styling/app.js
+++ Lecture-11 airbnb styling/app.js
@@ -16,6 +16,8 @@ app.use(express.urlencoded())
 app.use(homeRouter)
 app.use(contactRouter)
 
+app.use(express.static(path.join(rootDir, 'public')))
+
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
 })
